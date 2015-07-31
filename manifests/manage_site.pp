341d04406319e1ad7dafe50ef8421580aeb85894
jeffreybsmith <jeffrey.b.smith@gmail.com>
2015-07-31 10:56:02 -0500

    Adding support for a Site Id parameter

--- manifests/manage_site.pp
+++ manifests/manage_site.pp
@@ -7,7 +7,8 @@ define iis::manage_site(
   $host_header = '',
   $ip_address  = '*',
   $port        = '80',
-  $ssl         = false
+  $ssl         = false,
+  $id          = ''
   ) {
   validate_re($ensure, '^(present|installed|absent|purged)$', 'ensure must be one of \'present\', \'installed\', \'absent\', \'purged\'')
   validate_bool($ssl)
@@ -21,13 +22,34 @@ define iis::manage_site(
 
     $cmdSiteExists = "Test-Path \"IIS:\\Sites\\${site_name}\""
 
+    
+    if $id =~ Integer {
+      $createSwitches = ["-Name \"${site_name}\"",
+          "-Port ${port} -IP ${ip_address}",
+          "-HostHeader \"${host_header}\"",
+          "-PhysicalPath \"${site_path}\"",
+          "-ApplicationPool \"${app_pool}\"",
+          "-Id \"${id}\"",
+          "-Ssl:$${ssl}"]
+      
+      $switches = join($createSwitches,' ')
+      exec { "CreateSite-${site_name}" :
+        command   => "Import-Module WebAdministration; New-WebSite ${switches}",
+        onlyif    => "Import-Module WebAdministration; if((${$cmdSiteExists})) { exit 1 } else { exit 0 }",
+        provider  => 'powershell',
+        path      => $::path,
+        logoutput => true,
+        require   => [ Iis::Createpath["${site_name}-${site_path}"], Iis::Manage_app_pool[$app_pool] ],
+      }
+    }
+    else {
     $createSwitches = ["-Name \"${site_name}\"",
           "-Port ${port} -IP ${ip_address}",
           "-HostHeader \"${host_header}\"",
           "-PhysicalPath \"${site_path}\"",
           "-ApplicationPool \"${app_pool}\"",
           "-Ssl:$${ssl}"]
-
+          
     $switches = join($createSwitches,' ')
     exec { "CreateSite-${site_name}" :
       command   => "Import-Module WebAdministration; \$id = (Get-WebSite | foreach {\$_.id} | sort -Descending | select -first 1) + 1; New-WebSite ${switches} -ID \$id",
@@ -37,7 +59,7 @@ define iis::manage_site(
       logoutput => true,
       require   => [ Iis::Createpath["${site_name}-${site_path}"], Iis::Manage_app_pool[$app_pool] ],
     }
-
+  }
     exec { "UpdateSite-PhysicalPath-${site_name}":
       command   => "Import-Module WebAdministration; Set-ItemProperty \"IIS:\\Sites\\${site_name}\" -Name physicalPath -Value \"${site_path}\"",
       onlyif    => "Import-Module WebAdministration; if((${$cmdSiteExists}) -eq \$false) { exit 1 } if ((Get-ItemProperty \"IIS:\\Sites\\${site_name}\" physicalPath) -eq \"${site_path}\") { exit 1 } else { exit 0 }",
